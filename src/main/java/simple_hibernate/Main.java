a3e3e48214eefb737551dab1520397bdc1187d65
Alexander Shatov <alx18486@gmail.com>
2019-10-02 20:31:34 +0300

    exception handling added

--- src/main/java/simple_hibernate/Main.java
+++ src/main/java/simple_hibernate/Main.java
@@ -1,5 +1,8 @@
 package simple_hibernate;
 
+
+import javax.persistence.RollbackException;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -8,18 +11,28 @@ import simple_hibernate.domain.SimpleEntity;
 
 public class Main {
 	 
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws HibernateException{
 		 
-		 SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		 Session session = sessionFactory.openSession();
-		 Transaction transaction = session.beginTransaction();
-		 SimpleEntity entity = new SimpleEntity("first_property", 14, true);
-		 int entityId = (Integer)session.save(entity);
-		 transaction.commit();
-		 SimpleEntity entityFromDb = session.get(SimpleEntity.class, entityId);
-		 System.out.println(entityFromDb.getPropertyOne());
+		Transaction transaction = null; 
+		Session session = null; 
+		 
+		try {
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+		SimpleEntity entity = new SimpleEntity("first_property", 14, true);
+		int entityId = (Integer)session.save(entity);
+		SimpleEntity entityFromDb = session.get(SimpleEntity.class, entityId);
+		transaction.commit();
+		System.out.println(entityFromDb.getPropertyOne());
+		}catch (HibernateException | RollbackException | IllegalStateException e){
+			 if (transaction != null) {
+				 transaction.rollback();
+			 }
+		}finally{
+			if(session != null) {
+				session.close();
+			}
+		} 
 	 } 
-	 
-	 
-	
 }
